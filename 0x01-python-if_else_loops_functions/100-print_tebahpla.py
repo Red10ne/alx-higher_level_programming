da9d5c43eb968bb34bea8503c563f25144963793
Red10ne <redboukri@gmail.com>
2023-08-24 15:01:03 -0700

    revalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
     if i % 2 == 0:
-        print(chr(i + ord('a')).upper(), end='')
+        print("{}".format(chr(i + ord('a')).upper()), end='')
     else:
-        print(chr(i + ord('a')).lower(), end='')
+        print("{}".format(chr(i + ord('a')).lower()), end='')
