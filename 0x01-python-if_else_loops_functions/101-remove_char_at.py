a683fbc1385fd0bf6ccf095ed6e39716bc858668
Red10ne <redboukri@gmail.com>
2023-08-24 16:02:39 -0700

    rem char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for char in str:
-        remchar = str[:n] + str[n + 1:]
-    return (remchar)
+    for i in str:
+        return str[:n] + str[n+1:]
